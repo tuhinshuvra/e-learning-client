88811918c1a4f7536613a77d2295aadefa064d90
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-10-27 14:21:26 +0600

    updated checkout page

--- src/Pages/CheckOut/CheckOut.js
+++ src/Pages/CheckOut/CheckOut.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../Authentication/AuthProvider';
 import './CheckOut.css';
 
@@ -14,15 +15,41 @@ const CheckOut = () => {
             .then(data => setCourses(data))
     }, [])
 
+    let courseName;
+
+    if (selected == '01') {
+        courseName = 'JavaScript'
+    }
+    else if (selected == '02') {
+        courseName = 'ReactJS'
+    }
+    else if (selected == '03') {
+        courseName = 'MongDB'
+    }
+    else if (selected == '04') {
+        courseName = 'Java'
+    }
+    else if (selected == '05') {
+        courseName = 'MySQL'
+    }
+    else if (selected == '06') {
+        courseName = 'Python'
+    }
+    else if (selected == '07') {
+        courseName = 'PHP'
+    }
+    else if (selected == '08') {
+        courseName = 'Go'
+    }
 
 
 
 
     return (
-        <div>
-            <h2>This is checkout page </h2>
-            <p>Yours Selected Course ID - {selected}</p>
-            {/* <p>{name}</p> */}
+        <div className=''>
+            <h3 className=' text-center fw-bold text-success mt-5'>Welcome to checkout</h3>
+            <p className=' text-center fw-bold'>Yours Selected Course ID - {selected}, Name : {courseName} Learning </p>
+            <Link to="/"> <button className='btn btn-success'>Check Out</button> </Link>
         </div>
     );
 };
